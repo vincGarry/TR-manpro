bf89793f21f4fd88ff9b5da7d9c8e705979793b0
Yulius Gatot <30671290+yuliusgtt@users.noreply.github.com>
2020-07-02 18:48:38 +0700

    Update cobaload.php

--- cobaload.php
+++ cobaload.php
@@ -1,5 +1,5 @@
 <?php
-		// $tbcustomer = simplexml_load_file('data/tbcustomer.xml');
+		// coba auto increment xml
 		$tbcustomer = simplexml_load_file('data/tbcustomer.xml');
 		$maxid = max(array_map('intval',$tbcustomer->xpath("//id_customer")));
 		print_r($maxid);
