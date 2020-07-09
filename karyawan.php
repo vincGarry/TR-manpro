34e46f0088e52198c30a8f3b20d918def3bf84cc
Yulius Gatot <yulius.gtt@gmail.com>
2020-07-10 00:25:26 +0700

    halaman karyawan

---
+++ karyawan.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include('header.php'); ?>
+</head>
+
+<body class="">
+  <div class="wrapper ">
+    <?php include('sidebar.php'); ?>
+    <div class="main-panel">
+      <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg navbar-absolute fixed-top navbar-transparent">
+        <div class="container-fluid">
+          <div class="navbar-wrapper">
+            <div class="navbar-toggle">
+            </div>
+            <a class="navbar-brand" href="javascript:;">Penerima</a>
+          </div>
+      </nav>
+      <!-- End Navbar -->
+      <div class="content" style="height: 800px;">
+        <div class="card">
+            <div class="col-sm-12 col-sm-offset-2">
+            <table class="table table-bordered" style="margin-top:30px;">
+                <thead>
+                    <th>ID_USER</th>
+                    <th>username</th>
+                    <th>Password</th>
+                    <th>Nama</th>
+                    <th>Alamat</th>
+                    <th>Email</th>
+                    <th>Nomor Telepon</th>
+                    <th>pilihan</th>
+                </thead>
+                <tbody>
+                    <?php
+                    //load xml file
+                    $file = simplexml_load_file('data/tbuser.xml');
+                    
+                    foreach($file->user as $row){
+                        ?>
+                        <tr>
+                            <td><?php echo $row->id_user; ?></td>
+                            <td><?php echo $row->username; ?></td>
+                            <td><?php echo md5($row->password); ?></td>
+                            <td><?php echo $row->nama; ?></td>
+                            <td><?php echo $row->alamat; ?></td>
+                            <td><?php echo $row->email; ?></td>
+                            <td><?php echo $row->nomor_telepon; ?></td>
+                            <td>
+<!--                                 <a href="#edit_<?php //echo $row->id_customer; ?>" data-toggle="modal" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-edit"></span> Edit</a>
+                                <?php //include('modal_edit_customer.php'); ?>
+                                <a href="#delete_<?php // echo $row->id_customer; ?>" data-toggle="modal" class="btn btn-danger btn-sm"><span class="glyphicon glyphicon-trash"></span> Hapus</a>
+                                <?php //include('modal_delete_customer.php'); ?> -->
+                            </td>
+                        </tr>
+                        <?php
+                    }
+        
+                    ?>
+                </tbody>
+            </table>
+        </div>
+        </div>
+      </div>
+  <?php include('nama.php'); ?>
+    </div>
+  </div>
+  <!-- include php -->
+  <?php include('modal_tambah_data_pengiriman.php'); ?>
+  <?php include('footer.php'); ?>
+  
+</body>
+
+</html>
