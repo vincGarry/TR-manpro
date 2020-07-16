96fc6771e208d69db6485eaa65efa7aebc27b5dc
Yulius Gatot <yulius.gtt@gmail.com>
2020-07-16 22:54:46 +0700

    fix search penerima + tambah data untuk test

--- indexdatacustomer.php
+++ indexdatacustomer.php
@@ -71,11 +71,11 @@
                     <br>Penerima
                   </div>
                   <div class="form-group">
-                    <br><input class="form-control" type="text" id="myInput" onkeyup="myFunction()" placeholder="Cari berdasarkan ID Penerima..." >
+                    <br><input class="form-control" type="text" id="myInput1" onkeyup="myFunction1()" placeholder="Cari berdasarkan ID Penerima..." >
                   </div>
                 </div>
               </div>
-            <table id="myTable" class="table table-bordered" style="margin-top:30px;">
+            <table id="myTable1" class="table table-bordered" style="margin-top:30px;">
                 <thead>
                     <th>ID_penerima</th>
                     <th>Nama penerima</th>
@@ -117,6 +117,25 @@
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
   table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+function myFunction1() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput1");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable1");
   tr = table.getElementsByTagName("tr");
 
   for (i = 0; i < tr.length; i++) {
