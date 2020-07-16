d2a03fda3e510622272583cb287a454c72d2825a
laylafrdd <67739494+laylafrdd@users.noreply.github.com>
2020-07-17 01:05:19 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include('header.php'); ?>
+</head>
+
+<body class="">
+    <!-- <div class="wrapper ">
+    <?php include('sidebar_home.php'); ?>
+    <div class="main-panel"> -->  
+<br />
+<br />
+<br />
+ <h3 id="txt-welcome">JASA PENGIRIMAN</h3>
+
+ <h5 id="txt-isi"> Apa yang kami tawarkan?</h5>
+
+
+    <?php include('footer.php'); ?>
+
+    </body>
+    </html>
+    <?php include('nama.php'); ?>
+
+    
+ <style>
+   #txt-welcome {
+     text-align: center;
+     color: black;
+   }
+ </style>
+  <style>
+   #txt-isi {
+     text-align: center;
+     color: black;
+   }
+ </style>
---
+++ sidebar_home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<body class="">
+    <nav class="navbar navbar-dark bg-dark">
+  <!-- Navbar content -->
+  <a class="navbar-brand" href="home.php">Jasa Pengiriman</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="home.php">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="tracking.php">Tracking resi</a>
+      <a class="nav-item nav-link" href="#">About Us</a>
+      <a class="nav-item nav-link " href="index.php">Login</a>
+    </div>
+  </div>
+</nav>
+</nav>
+
+</body>
+</html>
