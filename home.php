c57f0b0f08a79637b162a2e5a091e8fb16747f1f
Yulius Gatot <yulius.gtt@gmail.com>
2020-07-17 07:09:02 +0700

    Update home.php

--- home.php
+++ home.php
@@ -4,24 +4,21 @@
 <head>
     <?php include('header.php'); ?>
 </head>
-
 <body class="">
-    <div class="wrapper ">
+    <div class="navbar-wrapper">
     <?php include('sidebar_home.php'); ?>
-    <div class="main-panel">
-<br />
-<br />
-<br />
+    </div>
+<div class="main-panel">
  <h3 id="txt-welcome">JASA PENGIRIMAN</h3>
 
  <h5 id="txt-isi"> Apa yang kami tawarkan?</h5>
 
     <?php include('footer.php'); ?>
 
+    <?php include('nama.php'); ?>
+</div>
     </body>
     </html>
-    <?php include('nama.php'); ?>
-
 <style>
    #txt-welcome {
      text-align: center;
