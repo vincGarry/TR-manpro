ee0deb2b7417eab6e8c9892c9cc733ef7054e962
laylafrdd <67739494+laylafrdd@users.noreply.github.com>
2020-07-23 19:32:52 +0700

    Update home.php

--- home.php
+++ home.php
@@ -3,31 +3,66 @@
 
 <head>
     <?php include('header.php'); ?>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body class="">
-    <div class="navbar-wrapper">
+
+<body>
     <?php include('sidebar_home.php'); ?>
-    </div>
-<div class="main-panel">
- <h3 id="txt-welcome">JASA PENGIRIMAN</h3>
+<br />
+<br />
+<br />
+ <h1 id="txt-welcome">JASA PENGIRIMAN</h1>
 
  <h5 id="txt-isi"> Apa yang kami tawarkan?</h5>
+ <br />
+ <br />
+ <div class="ftco-service-image-1 pb-5">
+          <div class="container">
+            <div class="owl-carousel owl-all">
+              <div class="service text-center">
+                <a href="#"><img src="kapal.jpg" alt="Image" class="img-fluid"></a>
+                <div class="px-md-3">
+                  <h3><a href="#">Pengiriman Jalur Laut</a></h3>
+                  <p>Kami menghadirkan pengiriman menggunakan jalur perairan yang beroperasi setiap hari</p>
+                </div>
+              </div>
+              <div class="service text-center">
+                <a href="#"><img src="pesawat.jpg" alt="Image" class="img-fluid"></a>
+                <div class="px-md-3">
+                  <h3><a href="#">Pengiriman Jalur Udara</a></h3>
+                  <p>Kami menghadrikan pengiriman menggunakan jalur udara yang beroperasi setiap hari</p>
+                </div>
+              </div>
+              <div class="service text-center">
+                <a href="#"><img src="delivery.jpg" alt="Image" class="img-fluid"></a>
+                <div class="px-md-3">
+                  <h3><a href="#">Pengiriman Jalur Darat</a></h3>
+                  <p>Kami menghadirkan pengiriman menggunakan jalur darat yang beroperasi setiap hari</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
-    <?php include('footer.php'); ?>
+      </div>
+      
 
-    <?php include('nama.php'); ?>
-</div>
-    </body>
-    </html>
-<style>
+ <style>
    #txt-welcome {
      text-align: center;
-     color: black;
+     color: white;
+     color: white;
    }
  </style>
   <style>
    #txt-isi {
      text-align: center;
-     color: black;
+     color: white;
+     color: white;
    }
  </style>
+    <?php include('footer.php'); ?>
+
+    </body>
+    <?php include('nama.php'); ?>
+    </html>
