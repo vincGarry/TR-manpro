6baff305b22f810dfcb194b1ebbcca47f161fe5d
vincGarry <57667004+vincGarry@users.noreply.github.com>
2020-07-23 22:21:06 +0700

    fix status

--- modal_delete_pengiriman.php
+++ modal_delete_pengiriman.php
@@ -1,16 +1,16 @@
 <div class="modal fade" id="hapus_<?php echo $row->id_pengiriman; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header">
+            <div class="header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                 <center><h4 class="modal-title" id="myModalLabel">Hapus pengiriman</h4></center>
             </div>
             <div class="modal-body">	
-            	<p class="text-center">Hapus pengirimane</p>
+            	<p class="text-center">Apakah anda yakin hapus pengiriman ?</p>
 				<h2 class="text-center"><?php echo $row->nama; ?></h2>
 			</div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Batal</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Batal</button> &nbsp;
                 <a href="delete_pengiriman.php?id_pengiriman=<?php echo $row->id_pengiriman; ?>" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Hapus</a>
             </div>
         </div>
